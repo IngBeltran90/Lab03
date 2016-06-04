02f6e8d45f08a0ea1cf596393182d7ea499e5500
Philbert <philbert@philberts-mac.local>
2016-06-04 15:00:34 -0500

    Revé

--- Lab03/Start.h
+++ Lab03/Start.h
@@ -12,10 +12,37 @@
 
 @property (strong, nonatomic) IBOutlet UIScrollView *Scroller1;
 @property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion2;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion3;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion4;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion5;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion6;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion7;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion8;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion9;
+@property (strong, nonatomic) IBOutlet UIImageView *ImgQuestion10;
 
 
 - (IBAction)Button1:(id)sender;
 - (IBAction)Button2:(id)sender;
+- (IBAction)Button3:(id)sender;
+- (IBAction)Button4:(id)sender;
+- (IBAction)Button5:(id)sender;
+- (IBAction)Button6:(id)sender;
+- (IBAction)Button7:(id)sender;
+- (IBAction)Button8:(id)sender;
+- (IBAction)Button9:(id)sender;
+- (IBAction)Button10:(id)sender;
+- (IBAction)Button11:(id)sender;
+- (IBAction)Button12:(id)sender;
+- (IBAction)Button13:(id)sender;
+- (IBAction)Button14:(id)sender;
+- (IBAction)Button15:(id)sender;
+- (IBAction)Button16:(id)sender;
+- (IBAction)Button17:(id)sender;
+- (IBAction)Button18:(id)sender;
+- (IBAction)Button19:(id)sender;
+- (IBAction)Button20:(id)sender;
 
 
 @end
--- Lab03/Start.m
+++ Lab03/Start.m
@@ -26,22 +26,81 @@
 
     -(void)viewDidAppear:(BOOL)animated {
         [self.Scroller1 setScrollEnabled:YES];
-        [self.Scroller1 setContentSize:CGSizeMake(0, 10000)];
+        [self.Scroller1 setContentSize:CGSizeMake(0, 2750)];
 }
 
     -(void)viewDidLayoutSubviews {
         [super viewDidLayoutSubviews];
-        [self.Scroller1 setContentSize:CGSizeMake(0, 10000)];
+        [self.Scroller1 setContentSize:CGSizeMake(0, 2750)];
 }
 
 
 - (IBAction)Button1:(id)sender {
     //self.LblTitle.text = @"Is Alive!";
-    self.ImgQuestion.image = [UIImage imageNamed: @"tick.png"];
+    self.ImgQuestion.image = [UIImage imageNamed: @"Wrong.png"];
+    self.editButtonItem.enabled = FALSE;
 }
 
 - (IBAction)Button2:(id)sender {
-    self.ImgQuestion.image = [UIImage imageNamed: @"Wrong.png"];
+    self.ImgQuestion.image = [UIImage imageNamed: @"tick.png"];
+}
+
+- (IBAction)Button3:(id)sender {
+    self.ImgQuestion2.image = [UIImage imageNamed: @"Wrong.png"];
+
+}
+
+- (IBAction)Button4:(id)sender {
+    self.ImgQuestion2.image = [UIImage imageNamed: @"tick.png"];
+
+}
+
+- (IBAction)Button5:(id)sender {
+}
+
+- (IBAction)Button6:(id)sender {
+}
+
+- (IBAction)Button7:(id)sender {
+}
+
+- (IBAction)Button8:(id)sender {
+}
+
+- (IBAction)Button9:(id)sender {
+}
+
+- (IBAction)Button10:(id)sender {
+}
+
+- (IBAction)Button11:(id)sender {
+}
+
+- (IBAction)Button12:(id)sender {
+}
+
+- (IBAction)Button13:(id)sender {
+}
+
+- (IBAction)Button14:(id)sender {
+}
+
+- (IBAction)Button15:(id)sender {
+}
+
+- (IBAction)Button16:(id)sender {
+}
+
+- (IBAction)Button17:(id)sender {
+}
+
+- (IBAction)Button18:(id)sender {
+}
+
+- (IBAction)Button19:(id)sender {
+}
+
+- (IBAction)Button20:(id)sender {
 }
 
 @end
